a66436593ee2d897a0d1d259f62d51b237c0c342
Denis Steckelmacher <steckdenis@logram-project.org>
2010-01-30 13:09:00 +0100

    Doxygen documentation for two files

--- libpackage/databaseformat.h
+++ libpackage/databaseformat.h
@@ -20,6 +20,29 @@
  * Boston, MA  02110-1301  USA
  */
 
+/**
+    @file databaseformat.h
+    @brief Description du format binaire de la base de donnée
+    
+    La base de donnée binaire de Setup est composée de plusieurs fichiers :
+    
+     - @b packages    : Contient la liste des paquets connus par Setup. Il contient
+                        une liste de _Package
+     - @b strings     : Contient une liste de _String. Ainsi, dans @b packages, toute
+                        référence à une chaîne se fait grâce à son index. La comparaison
+                        entre deux chaînes en est largement facilitée et accélérée.
+     - @b translate   : Même chose que @b strings, mais contient les chaînes traduites
+                        (descriptions des paquets)
+     - @b depends     : Liste de _Depend, permet de savoir quels paquets en nécessitent 
+                        d'autres
+     - @b strpackages : Permet de savoir à quels paquets appartient une chaîne. Par exemple,
+                        <em>libinitng</em> peut appartenir au paquet numéro 22 et au paquet
+                        numéro 45, c'est à dire par exemple libinitng~0.7.0 et
+                        libinitng~0.7.1. Ainsi, la résolution des dépendances est largement
+                        accélérée
+                                    
+*/
+
 #ifndef __DATABASEFORMAT_H__
 #define __DATABASEFORMAT_H__
 
@@ -28,62 +51,85 @@
 namespace Logram
 {
 
+/**
+    @brief Paquet dans la base de donnée binaire
+    
+    Cette grosse structure contient toutes les informations nécessaires à la gestion
+    des paquets dans la base de donnée binaire
+*/
 struct _Package
 {
-    int32_t name;       // Index de la chaîne du nom
-    int32_t version;    // Index de la chaîne de version
-    int32_t maintainer; // Index de la chaîne du nom du mainteneur
-    int32_t short_desc; // Index de la chaîne de description courte
-    int32_t source;     // Nom du paquet source
-    int32_t uurl;       // Url upstream (site web, etc)
-    int32_t repo;       // Nom du mirroir qui fourni le paquet (sources.list)
-    int32_t arch;       // Url de téléchargement
-    int32_t section;    // Section
-    int32_t distribution; // Distribution
-    int32_t license;    // Licence
-    int32_t pkg_hash;   // Index de la chaîne représentant le hash sha1 du paquet .tlz
-    int32_t mtd_hash;   // Index de la chaîne représentant le hash sha1 du fichier de métadonnées
-    int32_t flags;      // Flags
-    int32_t deps;       // Index du tableau de dépendances
-    int32_t isize;      // Taille de l'installation
-    int32_t dsize;      // Taille du téléchargement
+    int32_t name;       /*!< Index de la chaîne du nom */
+    int32_t version;    /*!< Index de la chaîne de version */
+    int32_t maintainer; /*!< Index de la chaîne du nom du mainteneur */
+    int32_t short_desc; /*!< Index de la chaîne de description courte */
+    int32_t source;     /*!< Nom du paquet source */
+    int32_t uurl;       /*!< Url upstream (site web, etc) */
+    int32_t repo;       /*!< Nom du mirroir qui fourni le paquet (sources.list) */
+    int32_t arch;       /*!< Url de téléchargement */
+    int32_t section;    /*!< Section */
+    int32_t distribution; /*!< Distribution */
+    int32_t license;    /*!< Licence */
+    int32_t pkg_hash;   /*!< Index de la chaîne représentant le hash sha1 du paquet .tlz */
+    int32_t mtd_hash;   /*!< Index de la chaîne représentant le hash sha1 du fichier de métadonnées */
+    int32_t flags;      /*!< Flags */
+    int32_t deps;       /*!< Index du tableau de dépendances */
+    int32_t isize;      /*!< Taille de l'installation */
+    int32_t dsize;      /*!< Taille du téléchargement */
 
-    int32_t idate;      // Timestamp de l'installation
-    int32_t iby;        // UID de l'utilisateur ayant installé le paquet
-    int32_t state;      // Status du paquet (PACKAGE_STATE)
+    int32_t idate;      /*!< Timestamp de l'installation */
+    int32_t iby;        /*!< UID de l'utilisateur ayant installé le paquet */
+    int32_t state;      /*!< Status du paquet (PACKAGE_STATE) */
 };
 
+/**
+    @brief Chaîne de caractère
+*/
 struct _String
 {
-    int32_t ptr;        // Pointeur à partir du début de la table des données
-    int32_t strpkg;     // Index d'un StrPackagePtr dans strpackages
+    int32_t ptr;        /*!< Pointeur à partir du début de la table des données */
+    int32_t strpkg;     /*!< Index d'un StrPackagePtr dans strpackages */
 };
 
+/**
+    @brief Permet, pour un paquet, d'avoir sa liste de dépendances
+    
+    En effet, _Package a un champs @b deps, qui est l'index d'un _DependPtr.
+    Ce _DependPtr permet d'accéder à la liste de toutes les dépendances du paquet
+*/
 struct _DependPtr
 {
-    int32_t ptr;        // Pointeur sur le premier Depend à partir du début des données
-    int32_t count;      // Nombre de Depend dans le tableau
+    int32_t ptr;        /*!< Pointeur sur le premier Depend à partir du début des données */
+    int32_t count;      /*!< Nombre de Depend dans le tableau */
 };
 
+/**
+    @brief Dépendance d'un paquet
+*/
 struct _Depend
 {
-    int8_t type;        // DEPEND_TYPE
-    int8_t op;          // DEPEND_OP
-    int32_t pkgname;    // Index de la chaîne du nom du paquet de la dépendance
-    int32_t pkgver;     // Index de la chaîne de la version du paquet de la dépendance
+    int8_t type;        /*!< Type de dépendace, voir DEPEND_TYPE_* */
+    int8_t op;          /*!< Opération (=, >=, etc), voir DEPEND_OP_* */
+    int32_t pkgname;    /*!< Index de la chaîne du nom du paquet de la dépendance */
+    int32_t pkgver;     /*!< Index de la chaîne de la version du paquet de la dépendance */
 };
 
-// Structure qui quand on a un index de string (un nom), et un index de version, permet de retrouver le paquet
+/**
+    @brief Permet de trouver le paquet à la bonne version possédant cette chaîne
+*/
 struct _StrPackage
 {
-    int32_t version;    // Index de la chaîne de version nécessaire pour qu'on ait le bon paquet
-    int32_t package;    // Index du paquet
+    int32_t version;    /*!< Index de la chaîne de version nécessaire pour qu'on ait le bon paquet */
+    int32_t package;    /*!< Index du paquet */
 };
 
+/**
+    @brief Permet, pour une chaîne, d'avoir la liste des paquets qui l'utilisent
+*/
 struct _StrPackagePtr
 {
-    int32_t ptr;        // Pointeur sur un StrPackage dans la zone de données
-    int32_t count;      // Nombre de StrPackages dedans
+    int32_t ptr;        /*!< Pointeur sur un _StrPackage dans la zone de données */
+    int32_t count;      /*!< Nombre de StrPackages dedans */
 };
 
 } /* Namespace */
--- libpackage/databasepackage.h
+++ libpackage/databasepackage.h
@@ -20,6 +20,11 @@
  * Boston, MA  02110-1301  USA
  */
 
+/**
+    @file databasepackage.h
+    @brief Contient la classe DatabasePackage, permettant de gérer les paquets en base de donnée
+*/
+
 #ifndef __DATABASEPACKAGE_H__
 #define __DATABASEPACKAGE_H__
 
@@ -41,60 +46,199 @@ struct ManagedDownload;
 struct _Depend;
 class DatabaseDepend;
 
+/**
+    @brief Paquet dans la base de donnée binaire
+    
+    Hérite de la classe Package et réimplémente son interface.
+    
+    Cette classe permet d'accéder facilement aux paquets contenus dans la base
+    de donnée binaire, ainsi que d'en extraire leurs informations, leurs 
+    dépendances, etc.
+    
+    Cette classe contient également quelques fonctions supplémentaires
+    permettant de faires plus de choses avec un paquet en base de donnée
+    qu'avec un paquet générique.
+*/
 class DatabasePackage : public Package
 {
     Q_OBJECT
     
     public:
+        /**
+            @brief Construit un nouveau DatabasePackage
+            @param index Index du paquet dans la base de donnée (fichier @b packages)
+            @param ps PackageSystem permettant de lier cette classe au système de gestion des paquets de Logram
+            @param psd Lecteur de base de donnée binaire permettant de récupérer les informations nécessaires
+            @param _action Action demandée au paquet, passée à Package::Package
+        */
         DatabasePackage(int index, PackageSystem *ps, DatabaseReader *psd, Solver::Action _action = Solver::None);
+        
+        /**
+            @brief Destructeur
+        */
         ~DatabasePackage();
 
+        /**
+            @brief Télécharge le paquet
+            
+            Télécharge le fichier .tlz du paquet binaire, et le place dans le cache Setup. 
+            
+            Cette fonction n'est pas bloquante, elle retourne sitôt le téléchargement commencé.
+            
+            Une fois le paquet téléchargé (downloadEnded() est émis), vous pouvez récupérer
+            le nom complet du fichier .tlz local en appelant tlzFileName().
+            
+            @sa tlzFileName
+            @return true si le téléchargement est bien lancé, false sinon
+        */
         bool download();
+        
+        /**
+            @brief Nom du fichier .tlz récupéré
+            
+            Une fois le fichier .tlz du paquet téléchargé par download(), appelez cette fonction
+            pour récupérer son nom.
+            
+            @sa download
+            @return Nom du fichier .tlz téléchargé
+        */
         QString tlzFileName();
+        
+        /**
+            @brief Permet de savoir si le paquet est valide
+            
+            Il se peut que des erreurs se soient produites dans le constructeur, qui ne peut
+            pas retourner une valeur (false ici). Cette fonction renvoie false si le paquet
+            n'a pu être construit. PackageSystem::lastError() est placé de manière appropriée.
+            
+            @return true si le paquet est prêt à être utilisé, false sinon
+        */
         bool isValid();
+        
+        /**
+            @brief Renvoie l'origine du paquet, Package::Database ici
+            
+            Permet de savoir si un Package vient de la base de donnée ou d'un fichier.
+            L'implémentation dans cette classe renvoie toujours Package::Database.
+            
+            @return Package::Database
+        */
         Package::Origin origin();
 
+        /**
+            @brief Type d'url d'un paquet
+            
+            Un paquet dans la base de donnée doit être téléchargé. Il possède néanmoins plusieurs
+            urls différentes (paquet .tlz, métadonnées, source). Cette énumération vous permet
+            de demander la bonne url à url().
+            
+            @sa url
+        */
         enum UrlType
         {
-            Binary,
-            Metadata,
-            Source
+            Binary,         /*!< Url du paquet binaire .tlz */
+            Metadata,       /*!< Url des métadonnées .xml.xz */
+            Source          /*!< Url de la source .src.tlz */
         };
         
-        QString name();
-        QString version();
-        QString newerVersion();
-        QString maintainer();
-        QString shortDesc();
-        QString source();
-        QString upstreamUrl();
-        QString repo();
-        QString section();
-        QString distribution();
-        QString license();
-        QString arch();
+        QString name();             /*!< Nom du paquet */
+        QString version();          /*!< Version du paquet */
+        QString newerVersion();     /*!< Version d'une éventuelle mise à jour, QString() si paquet à jour */
+        QString maintainer();       /*!< Mainteneur du paquet */
+        QString shortDesc();        /*!< Description courte */
+        QString source();           /*!< Nom du paquet source (libinitng vient d'initng) */
+        QString upstreamUrl();      /*!< Url du site web du projet à la base de l'application empaquetée */
+        QString repo();             /*!< Dépôt duquel vient le paquet */
+        QString section();          /*!< Section du paquet (base, devel, games, etc) */
+        QString distribution();     /*!< Distribution du paquet (experimental, stable, old, testing) */
+        QString license();          /*!< License du paquel (GPLv2, GPLv3, BSD, Apache, etc) */
+        QString arch();             /*!< Architecture du paquet (i686, x86_64, all, src) */
+        
+        /** 
+            @brief Récupère l'url d'un paquet
+            
+            Un paquet dans la base de donnée doit être téléchargé. Il possède néanmoins plusieurs
+            urls différentes (paquet .tlz, métadonnées, source). Cette fonction vous permet de la
+            calculer en fonction du nom, de la version et de l'architecture du paquet, ainsi
+            que de l'architecture standard d'un dépôt Logram.
+            
+            @warning L'url ne contient pas le dépôt. Elle se présente sous la forme "pool/i/initng/...".
+                     Vous devez vous-même récupérer l'url racine du dépôt du paquet, avec ses mirroirs.
+            
+            @sa UrlType
+            @return Url du paquet, calculée correctement.
+        */
         QString url(UrlType type = DatabasePackage::Binary);
-        QByteArray packageHash();
-        QByteArray metadataHash();
-        int flags();
+        QByteArray packageHash();   /*!< Hash SHA1 du fichier .tlz, pour vérifier son authenticité */
+        QByteArray metadataHash();  /*!< Hash SHA1 des métadonnées du paquet */
+        int flags();                /*!< Flags du paquet */
+        
+        /**
+            @brief Définis les flags d'un paquet
+            
+            Une fois un paquet en base de donnée (donc venant des dépôts ou d'un fichier installé),
+            l'utilisateur a la possibilité de lui attribuer des flags (ne pas supprimer, etc).
+            
+            Cette fonction vous permet de définir les flags d'un paquet, en utilisant les définitions
+            se trouvant dans le fichier package.h.
+            
+            @code
+                DatabasePackage *pkg = getPackage();
+                
+                int flag = PACKAGE_FLAG_DONTUPDATE;
+                
+                pkg->setFlags(pkg->flags() | flag);
+            @endcode
+            
+            @param flags nouveau flags du paquet
+            @sa flags
+        */
         void setFlags(int flags);
         
-        QDateTime installedDate();
-        int installedBy();
-        int status();
+        QDateTime installedDate();  /*!< Date d'installation du paquet, indéfini si non-installé */
+        int installedBy();          /*!< UID de l'utilisateur ayant installé le paquet */
+        int status();               /*!< Status du paquet, voir PACKAGE_STATE_* dans package.h */
         
-        int downloadSize();
-        int installSize();
+        int downloadSize();         /*!< Taille du fichier .tlz à télécharger, en octets */
+        int installSize();          /*!< Taille du paquet une fois installée. PackageSystem::fileSizeFormat pour formatter tout ça) */
 
-        QList<Package *> versions();
-        QList<Depend *> depends();
+        QList<Package *> versions();    /*!< Liste de Package donc chacun est une version différente de ce paquet */
+        QList<Depend *> depends();      /*!< Liste de Depend représentant les dépendances de ce paquet */
         
+        /**
+            @brief Enregistrer l'état du paquet
+            @internal
+            
+            Quand un paquet est installé, il doit être enregistré dans la base de donnée binaire comme l'étant.
+            Cette fonction le fait
+            
+            @param idate : TimeStamp UNIX de la date d'installation
+            @param iby : UID de l'utilisateur ayant installé le paquet
+            @param state : Status du paquet (installé, supprimé, autre)
+        */
         void registerState(int idate, int iby, int state);
 
     signals:
+        /**
+            @brief Le paquet est téléchargé
+            
+            Émis lorsque le paquet a fini d'être télécharge. tlzFileName contient alors le nom d'un fichier
+            .tlz dans le système de fichier local.
+            
+            @sa tlzFileName
+            @param success true si le téléchargement a réussi en entier, false sinon
+        */
         void downloaded(bool success);
 
     private slots:
+        /**
+            @brief Téléchargement du fichier terminé
+            @internal
+            
+            Le fichier .tlz est téléchargé. Ce slot émet downloaded
+            
+            @param md Logram::ManagedDownload représentant le téléchargement
+        */
         void downloadEnded(Logram::ManagedDownload *md);
 
     private:
@@ -102,6 +246,11 @@ class DatabasePackage : public Package
         Private *d;
 };
 
+/**
+    @brief Dépendance d'un paquet en base de donnée
+    
+    Cette classe implémente l'interface Depend et s'utilise de la même manière
+*/
 class DatabaseDepend : public Depend
 {
     public:
