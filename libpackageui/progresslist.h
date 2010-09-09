a239b3b0047548f7484c78db9a3b890df15ea85f
Denis Steckelmacher <steckdenis@logram-project.org>
2010-09-09 13:56:07 +0200

    Document ProgressList.

--- libpackageui/progresslist.h
+++ libpackageui/progresslist.h
@@ -23,6 +23,11 @@
 #ifndef __PROGRESSLIST_H__
 #define __PROGRESSLIST_H__
 
+/**
+ * @file progresslist.h
+ * @brief Liste des progressions
+ */
+
 #include <QWidget>
 
 namespace Logram
@@ -32,20 +37,77 @@ namespace Logram
 
 namespace LogramUi
 {
-    
+
+/**
+ * @brief Liste des progressions
+ * 
+ * Les progressions dans Setup sont puissantes mais un peu complexes à gérer
+ * pour les applications clientes.
+ * 
+ * Ce widget permet d'afficher un ensemble de barres de progressions en
+ * fonction des progressions envoyées par le PackageSystem.
+ * 
+ * Pour les téléchargements, la vitesse de téléchargement et la taille
+ * téléchargée/restante est affichée.
+ */
 class ProgressList : public QWidget
 {
     Q_OBJECT
     public:
+        /**
+         * @brief Constructeur
+         * @param parent Parent
+         */
         ProgressList(QWidget *parent);
+        
+        /**
+         * @brief Destructeur
+         */
         ~ProgressList();
         
+        /**
+         * @brief Ajoute une progression
+         * 
+         * Cette fonction peut être appelée pour ajouter toute progression
+         * que vous décidez de faire gérer par cette classe.
+         * 
+         * @code
+         * if (progress->action == Progress::Create)
+         * {
+         *     progressDialog->addProgress(progress);
+         * }
+         * else if (progress->action == Progress::Update)
+         * {
+         *     progressDialog->updateProgress(progress);
+         * }
+         * else
+         * {
+         *     progressDialog->endProgress(progress);
+         * }
+         * @endcode
+         * 
+         * @warning Toute progression qui sera passée comme paramètre à
+         *          updateProgress() ou endProgress() doit d'abord avoir
+         *          été envoyée à cette fonction.
+         * 
+         * @param progress Progression à ajouter
+         */
         void addProgress(Logram::Progress *progress);
+        
+        /**
+         * @brief Mettre à jour une progression
+         * @param progress Progression
+         */
         void updateProgress(Logram::Progress *progress);
+        
+        /**
+         * @brief Termine une progression
+         * @param progress Progression
+         */
         void endProgress(Logram::Progress *progress);
         
-        int count() const;
-        void clear();
+        int count() const;      /*!< @brief Nombre de progressions gérées */
+        void clear();           /*!< @brief Efface toutes les progressions */
         
     private:
         struct Private;
