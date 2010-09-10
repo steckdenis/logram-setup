3b4e064b0c26f97cc4c32ae5cd9b81b05b7bb5ff
Denis Steckelmacher <steckdenis@logram-project.org>
2010-09-10 16:39:02 +0200

    Document InstallWizard.

--- libpackageui/installwizard.h
+++ libpackageui/installwizard.h
@@ -23,6 +23,11 @@
 #ifndef __INSTALLWIZARD_H__
 #define __INSTALLWIZARD_H__
 
+/**
+ * @file installwizard.h
+ * @brief Assistant d'installation de paquets
+ */
+
 #include <QWizard>
 #include <QVector>
 
@@ -44,13 +49,34 @@ class DonePage;
 namespace LogramUi
 {
 
+/**
+ * @brief Message laissé par un paquet
+ * 
+ * La dernière page de l'assistant d'installation contient un récapitulatif
+ * de tout ce qui s'est passé lors de l'installation.
+ * 
+ * Cette classe permet d'ajouter un message sur cette page. Ce sont généralement
+ * les autres pages de l'assistant qui font cela, mais il est également possible
+ * d'ajouter des messages généraux avant de l'afficher.
+ */
 struct PackageMessage
 {
-    Logram::Package *pkg;
-    QString title;
-    QString message;
+    Logram::Package *pkg;       /*!< @brief Paquet duquel vient le message, 0 pour général */
+    QString title;              /*!< @brief Titre du message */
+    QString message;            /*!< @brief Contenu du message */
 };
 
+/**
+ * @brief Assistant d'installation de paquets
+ * 
+ * Cette classe propose à l'utilisateur un assistant d'installation de paquets.
+ * 
+ * Il suffit à l'application cliente de lui fournir un ensemble de paquets
+ * sélectionnés, et InstallWizard se chargera de les montrer à l'utilisateur, 
+ * lui faire valider les licences, choisir les dépendances, afficher les
+ * progressions et finalement une page de conclusion avec les différents
+ * messages laissés par les paquets ou par PackageSystem.
+ */
 class InstallWizard : public QWizard
 {
     Q_OBJECT
@@ -62,19 +88,32 @@ class InstallWizard : public QWizard
     friend class ::DonePage;
     
     public:
+        /**
+         * @brief Constructeur
+         * @param ps PackageSystem utilisé
+         * @param parent QWidget parent
+         */
         InstallWizard(Logram::PackageSystem *ps, QWidget *parent);
+        
+        /**
+         * @brief Destructeur
+         */
         ~InstallWizard();
         
-        void addPackage(Logram::Package *package);
-        QVector<Logram::Package *> packages() const;
+        void addPackage(Logram::Package *package);      /*!< @brief Ajoute un paquet à la liste */
+        QVector<Logram::Package *> packages() const;    /*!< @brief Liste des paquets dans la liste */
+        void addMessage(const PackageMessage &message); /*!< @brief Ajoute un message à la page de résumé */
         
+        /**
+         * @brief IDs des pages de l'assistant
+         */
         enum PageIds
         {
-            Actions,
-            Branches,
-            Licenses,
-            Install,
-            Done
+            Actions,                                    /*!< @brief Résumé des actions (paquets à installer) */
+            Branches,                                   /*!< @brief Choix des dépendances */
+            Licenses,                                   /*!< @brief Acceptation des éventuelles licences */
+            Install,                                    /*!< @brief Installation (affichage des progressions) */
+            Done                                        /*!< @brief Page de résumé après installation */
         };
         
     private:
@@ -87,7 +126,6 @@ class InstallWizard : public QWizard
         void setSolver(Logram::Solver *solver);
         void setPackageList(Logram::PackageList *packageList);
         
-        void addMessage(const PackageMessage &message);
         QList<PackageMessage> messages() const;
         
     private slots:
