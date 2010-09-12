8cad97173227901e1f4219aaf5908d9ddb8fbf0f
Denis Steckelmacher <steckdenis@logram-project.org>
2010-09-12 09:53:12 +0200

    Document InfoPane.

--- libpackageui/infopane.h
+++ libpackageui/infopane.h
@@ -23,6 +23,11 @@
 #ifndef __INFOPANE_H__
 #define __INFOPANE_H__
 
+/**
+ * @file infopane.h
+ * @brief Affichage d'informations sur un paquet
+ */
+
 #include <QTabWidget>
 
 class Ui_infoPane;
@@ -32,15 +37,52 @@ namespace LogramUi
 
 class PackageDataProviderInterface;
 
+/**
+ * @brief Affichage d'informations sur un paquet
+ * 
+ * Ce widget affiche un ensemble d'onglets comprenant diverses informations sur
+ * le paquet qu'on lui demande d'afficher.
+ * 
+ *  - Détails simples (nom, version, titre, versions disponibles, dépôt, etc)
+ *  - Description longue
+ *  - Dépendances
+ *  - Historique
+ *  - Fichiers (sous forme d'arbre, avec leurs flags)
+ * 
+ * Les informations sur le paquet sont obtenues à partir d'un
+ * PackageDataProviderInterface , ce qui vous permet d'afficher des
+ * informations même pour ce qui n'est pas vraiment un paquet.
+ */
 class InfoPane : public QTabWidget
 {
     Q_OBJECT
     
     public:
+        /**
+         * @brief Constructeur
+         * @param parent Parent
+         */
         InfoPane(QWidget *parent);
+        
+        /**
+         * @brief Destructeur
+         */
         ~InfoPane();
         
+        /**
+         * @brief Affiche les informations sur un paquet
+         * @param data Fournisseur de données
+         */
         void displayData(PackageDataProviderInterface *data);
+        
+        /**
+         * @brief Définit s'il faut afficher le panneau des versions dans le premier onglet
+         * 
+         * Ce panneau peut être inutile si vous savez qu'il n'y aura toujours
+         * qu'une seule version d'affichée.
+         * 
+         * @param show True pour afficher le paneau, false sinon.
+         */
         void setShowVersions(bool show);
         
     private slots:
