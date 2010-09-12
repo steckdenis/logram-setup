d21350474b0443222ab3f73fc1955e5040c87e79
Denis Steckelmacher <steckdenis@logram-project.org>
2010-09-12 09:54:07 +0200

    Document FilterInterface.

--- libpackageui/filterinterface.h
+++ libpackageui/filterinterface.h
@@ -23,48 +23,108 @@
 #ifndef __FILTERINTERFACE_H__
 #define __FILTERINTERFACE_H__
 
+/**
+ * @file filterinterface.h
+ * @brief Interface de filtrage des paquets
+ */
+
 #include <QObject>
 #include <QRegExp>
 
 namespace LogramUi
 {
 
+/**
+ * @brief Interface de filtrage des paquets
+ * 
+ * Cette classe propose une interface générique pour filtrer des paquets.
+ * Elle permet aux développeurs d'applications clientes de n'avoir à 
+ * surveiller qu'un seul objet pour filtrer les paquets aussi bien par
+ * nom (SearchBar) que par catégorie ou distribution (CategoryView).
+ * 
+ * Pour utiliser cette classe, il vous suffit de créer un objet de type
+ * FilterInterface, de le passer en paramètre aux constructeurs des
+ * interfaces de filtre que vous voulez utiliser, puis de connecter son
+ * signal dataChanged().
+ * 
+ * @code
+ * void init()
+ * {
+ *     filterInterface = new FilterInterface(this);
+ *     connect(filterInterface, SIGNAL(dataChanged()), this, SLOT(searchPackages()));
+ *    
+ *     searchBar = new SearchBar(filterInterface, this);
+ *     sections = new CategoryView(ps, filterInterface, this);
+ * }
+ * 
+ * void searchPackages()
+ * {
+ *     // Utiliser filterInterface->regex() par exemple
+ * }
+ * @endcode
+ */
 class FilterInterface : public QObject
 {
     Q_OBJECT
     
     public:
+        /**
+         * @brief Constructeur
+         * @param parent Objet parent
+         */
         FilterInterface(QObject *parent);
+        
+        /**
+         * @brief Destructeur
+         */
         ~FilterInterface();
         
+        /**
+         * @brief Filtres en fonction du status des paquets
+         * 
+         * Cette énumération permet de filtrer les paquets en fonction de leur
+         * status, pour par exemple n'afficher que les paquets installés.
+         */
         enum StatusFilter
         {
-            NoFilter = 0,
-            Installed = 1,
-            NotInstalled = 2,
-            Updateable = 3,
-            Orphan = 4
+            NoFilter = 0,           /*!< @brief Ne filtre pas les paquets */
+            Installed = 1,          /*!< @brief Afficher uniquement les paquets installés */
+            NotInstalled = 2,       /*!< @brief Afficher uniquement les paquets non-installés */
+            Updateable = 3,         /*!< @brief Afficher uniquement les paquets pouvant être mis à jour */
+            Orphan = 4              /*!< @brief Afficher uniquement les paquets orphelins */
         };
         
-        void setNamePattern(const QString &pattern);
-        void setNameSyntax(QRegExp::PatternSyntax syntax);
-        void setStatusFilter(StatusFilter filter);
-        void setRepository(const QString &repository);
-        void setDistribution(const QString &distribution);
-        void setSection(const QString &section);
+        void setNamePattern(const QString &pattern);        /*!< @brief Définit le motif de recherche */
+        void setNameSyntax(QRegExp::PatternSyntax syntax);  /*!< @brief Définit le type de recherche */
+        void setStatusFilter(StatusFilter filter);          /*!< @brief Définit le filtre en fonction du status des paquets */
+        void setRepository(const QString &repository);      /*!< @brief Définit le dépôt dans lequel doivent se trouver les paquets */
+        void setDistribution(const QString &distribution);  /*!< @brief Définit la distribution des paquets */
+        void setSection(const QString &section);            /*!< @brief Définit la section des paquets */
         
+        /**
+         * @brief Déclenche la mise à jour des vues
+         * 
+         * Demande à FilterInterface d'émettre le signal dataChanged().
+         */
         void updateViews();
         
-        QRegExp regex() const;
-        QRegExp::PatternSyntax nameSyntax() const;
-        QString namePattern() const;
-        StatusFilter statusFilter() const;
-        QString repository() const;
-        QString distribution() const;
-        QString section() const;
-        QString statusName(StatusFilter status) const;
+        QRegExp regex() const;                              /*!< @brief Expression régulière de recherche */
+        QRegExp::PatternSyntax nameSyntax() const;          /*!< @brief Syntaxe de cette expression */
+        QString namePattern() const;                        /*!< @brief Motif de cette expression (QString() si pas de filtre) */
+        StatusFilter statusFilter() const;                  /*!< @brief Filtre en fonction du status */
+        QString repository() const;                         /*!< @brief Dépôt des paquets (QString() si pas de filtre) */
+        QString distribution() const;                       /*!< @brief Distribution des paquets (QString() si pas de filtre) */
+        QString section() const;                            /*!< @brief Section des paquets (QString() si pas de filtre) */
+        QString statusName(StatusFilter status) const;      /*!< @brief Nom traduit du status */
         
     signals:
+        /**
+         * @brief Les données ont changé
+         * 
+         * Ce signal est émis si un widget de modification du filtre a appelé
+         * la fonction updateViews(). Les vues doivent alors re-filtrer leurs
+         * paquets.
+         */
         void dataChanged();
         
     private:
